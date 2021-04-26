d736be69291d627d8853fe34feffd56da43dc2da
Jainam Sanghvi <jainam1224@gmail.com>
2021-04-26 11:24:38 +0530

    Car App

---
+++ cars/api/serializes.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from cars.models import Cars
+
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cars
+        fields = ['id', 'car_brand','car_model','production_year','car_body','engine_type']
